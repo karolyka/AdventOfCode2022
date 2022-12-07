2d44a3e34e57b55f32f27fb35ee670e9da12204b
Karoly Machalik <karoly_machalik@epam.com>
2022-12-07 09:03:17 +0100

    Day 04

---
+++ day04/src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+import java.io.File
+
+const val INPUT_FILE_NAME = "day04/input.txt"
+//const val INPUT_FILE_NAME = "day04/input_test.txt"
+
+fun main(args: Array<String>) {
+
+    val lines = File(INPUT_FILE_NAME).readLines()
+
+    val rangePairs = lines.map { it.toRangePair() }
+
+    val coveredCount = rangePairs.count { it.isCovered() }
+    println("Fully covered pairs: $coveredCount")
+
+    val overlappedCount = rangePairs.count { it.isOverlapped() }
+    println("Overlapped pairs: $overlappedCount")
+}
+
+private fun String.toRangePair() = this.split(',').map { group ->
+    group.split('-').map { it.toInt() }.let { (it[0]..it[1]) }
+}.let { it[0] to it[1] }
+
+private fun Pair<IntRange, IntRange>.isCovered() =
+    (first.first <= second.first && first.last >= second.last) ||
+            (second.first <= first.first && second.last >= first.last)
+
+private fun Pair<IntRange, IntRange>.isOverlapped() =
+    !(second.last < first.first || second.first > first.last)
