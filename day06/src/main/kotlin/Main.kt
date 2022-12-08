8ed5fabeb94ee07503a71b96cdff26a371219299
Karoly Machalik <karoly_machalik@epam.com>
2022-12-08 09:28:18 +0100

    Day 06

--- day06/src/main/kotlin/Main.kt
+++ day06/src/main/kotlin/Main.kt
@@ -1,23 +1,19 @@
 import java.io.File
 
-//const val INPUT_FILE_NAME = "day06/input.txt"
-const val INPUT_FILE_NAME = "day06/input_test.txt"
+const val INPUT_FILE_NAME = "day06/input.txt"
+//const val INPUT_FILE_NAME = "day06/input_test.txt"
 
-private const val CHAR_NUM = 4
-private const val CHAR_NUM2 = 14
+private const val CHAR_NUM_TASK1 = 4
+private const val CHAR_NUM_TASK2 = 14
 
 fun main(args: Array<String>) {
     val lines = File(INPUT_FILE_NAME).readLines()
 
-    println("Task 1")
-
-    lines.forEach { line ->
-        println(line.windowed(CHAR_NUM).indexOfFirst { it.toSet().size == CHAR_NUM } + CHAR_NUM)
-    }
-
-    println("Task 2")
-
-    lines.forEach { line ->
-        println(line.windowed(CHAR_NUM2).indexOfFirst { it.toSet().size == CHAR_NUM2 } + CHAR_NUM2)
-    }
+    listOf(CHAR_NUM_TASK1, CHAR_NUM_TASK2)
+        .forEachIndexed { index, charNum ->
+            println("Task $index")
+            lines.forEach { line ->
+                println(line.windowed(charNum).indexOfFirst { it.toSet().size == charNum } + charNum)
+            }
+        }
 }
